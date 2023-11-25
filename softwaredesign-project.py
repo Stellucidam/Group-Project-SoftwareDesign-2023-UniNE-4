3878386ea49882e47e5bb8e0bd30e899164b36cb
Clarisse Fleurimont <clarisse.fleurimont@heig-vd.ch>
2023-11-25 09:56:48 +0100

    Change stuff

--- softwaredesign-project.py
+++ softwaredesign-project.py
@@ -1,72 +1,163 @@
 import pygame
-import random
+import sys
 from PIL import Image, ImageDraw
 
-WHITE = (255, 255, 255)
-WINDOWWIDTH = 800
-WINDOWHEIGHT = 600
-FPS = 60
+# Initialisation de Pygame
+pygame.init()
 
+# Définition des couleurs
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BRAWN = (165, 42, 42)
+
+# Format de la fenêtre
+WINDOWWIDTH = 744
+WINDOWHEIGHT = 420
+FPS = 30
 windowsurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
 pygame.display.set_caption("Run Greta, RUN")
+
+######################
+#accueil
+
+# Charger l'image de fond
+#fond = pygame.image.load("fond.png")
+#fond = pygame.transform.scale(fond, (largeur, hauteur))
+
+# Charger l'image du bouton "Start"
+#bouton_start = pygame.image.load("chemin/vers/votre/bouton_start.png")
+#bouton_start_rect = bouton_start.get_rect()
+#bouton_start_rect.x = (largeur - bouton_start_rect.width) // 2
+#bouton_start_rect.y = hauteur // 2
+
+# Boucle principale de la page d'accueil
+#while True:
+#for event in pygame.event.get():
+#if event.type == pygame.QUIT:
+#pygame.quit()
+#sys.exit()
+#elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+# Vérifier si le bouton "Start" a été cliqué
+#if bouton_start_rect.collidepoint(event.pos):
+# Lancer le jeu ici (remplacez cela par le code de votre jeu)
+#print("Le jeu est lancé !")
+
+# Afficher l'image de fond
+#fenetre.blit(fond, (0, 0))
+
+# Afficher le bouton "Start"
+#fenetre.blit(bouton_start, bouton_start_rect.topleft)
+
+# Mettre à jour l'affichage
+#pygame.display.flip()
+##########################################
+
+# Chargement de l'image de fond
+background = pygame.image.load("fond.png")
+background_x = 0
+
+# Score initial
 score = 0
 
-def afficher_score():
-    print("Score actuel :", score)
 
-pygame.init()
+# Fonction pour afficher le score
+def afficher_score():
+  font = pygame.font.Font(None, 36)  # Police et taille du texte
+  texte_score = font.render("Score : {}".format(score), True, WHITE)
+  text_rect = texte_score.get_rect(center=(WINDOWWIDTH // 2, 20))
+  windowsurface.blit(texte_score, (10, 10))  # Position du texte à l'écran
+
+
+# Afficher Trump
+Trump = pygame.image.load("trump.png")
+Trump = pygame.transform.scale(Trump, (200, 150))
+trump_rect = Trump.get_rect()
+trump_rect.topright = (WINDOWWIDTH - 50, 10)
+
+#Afficher Greta
+Greta = pygame.image.load("Greta.png")
+Greta = pygame.transform.scale(Greta, (200, 200))
+greta_rect = Greta.get_rect()
+greta_rect.center = (70, 20)
+# Variables du saut
+jumping = False
+jump_count = 10
+# Gravité
+gravity = 1
+
+# Boucle principale
+clock = pygame.time.Clock()
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+    #running
+    elif event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_SPACE and not jumping:
+        jumping = True
+
+  # Défilement de l'image de fond
+  background_x -= 1
+  if background_x < -background.get_width():
+    background_x = 0
+
+  #Afficher Trump
+  windowsurface.blit(Trump, trump_rect)
+  pygame.display.flip()
+
+  #Afficher Greta
+  windowsurface.blit(Greta, greta_rect)
+  pygame.display.flip()
+
+  # Gestion du saut
+  if jumping:
+    if jump_count >= -10:
+      neg = 1
+      if jump_count < 0:
+        neg = -1
+      greta_rect.y -= (jump_count**2) * 0.5 * neg
+      jump_count -= 1
+    else:
+      jumping = False
+      jump_count = 10
+
+  # Appliquer la gravité
+  if greta_rect.y < WINDOWHEIGHT - 50:
+    greta_rect.y += gravity
+  else:
+    greta_rect.y = WINDOWHEIGHT - 50
+
+  # Effacer l'écran
+  windowsurface.fill(BLACK)
+
+  # Dessiner nl'image de fond
+  windowsurface.blit(background, (background_x, 0))
+  windowsurface.blit(background, (background_x + background.get_width(), 0))
+  # Afficher le score
+  afficher_score()
+  # Mettre à jour l'affichage
+  pygame.display.flip()
+  # Réguler la vitesse de la boucle
+  clock.tick(FPS)
+
+
+#
+#
+#
+def set_difficulty(
+):  # function to progressively increase the speed of the background movement
+  global score
+  global background_speed
+  speed_modification = score // 3
+  background_speed = 3 + speed_modification
+
+
+#
+#
+#
 pygame.mouse.set_visible(False)
 
-Trump = Image.open("trump.png")
-cadre_x = 500
-cadre_y = 300
-cadre_largeur = 100
-cadre_hauteur = 100
-draw = ImageDraw.Draw(Trump)
-draw.rectangle((cadre_x, cadre_y, cadre_x + cadre_largeur, cadre_y + cadre_hauteur), outline="red", width=0)
-Trump.show()
-
-background = pygame.image.load("fond.png")
-clock = pygame.time.Clock()
-background_x = 0
-running = True
-
-class Player:
-    def __init__(self):
-        self.image = pygame.image.load("Greta.png")
-        self.rect = self.image.get_rect()
-
-    def movement(self, position):
-        self.rect.centerx = position[0]
-        self.rect.centery = position[1]
-
-class Enemy:
-    def __init__(self):
-        self.dead = False
-        self.image = random.choice([missile1, missile2, bomb])  # Assuming missile1, missile2, and bomb are defined
-        self.rect = self.image.get_rect()
-        self.speed = [random.uniform(-3, 3), 2]
-
-    def enemy_behaviour(self):
-        self.rect.move_ip(self.speed)
-        if self.rect.top < 0:
-            self.speed[1] = -self.speed[1]
-        elif self.rect.left < 0 or self.rect.right > WINDOWWIDTH:
-            self.speed[0] = -self.speed[0]
-        elif self.rect.bottom > WINDOWHEIGHT - player.rect.height:  # Adjusted to use player.rect.height
-            self.dead = True
-
-player = Player()
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Défilement de l'image de fond
-    background_x -= 1
-    windowsurface.blit(background, (background_x, 0))
-    windowsurface.blit(background, (background_x + background.get_width(), 0))
-    pygame.display.update()
-    clock.tick(60)  # Changed to a more reasonable value
 pygame.quit()
+sys.exit()
